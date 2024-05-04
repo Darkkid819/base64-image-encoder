35ad3b238f832f10a8467f50c71016e069c55c96
Darkkid819 <darkkid8192004@gmail.com>
2024-05-03 23:19:19 -0400

    changed layout

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ window.onload = function() {
     const fileInput = document.getElementById('file-input');
     const image = document.getElementById('image');
     const linkOutput = document.getElementById('link-output');
+    const uploadSection = document.getElementById('upload-section');
 
     fileInput.addEventListener('change', function(event) {
         const file = event.target.files[0];
@@ -12,6 +13,8 @@ window.onload = function() {
             window.location.hash = encodeURIComponent(base64String);
             linkOutput.textContent = window.location.href;
             image.src = base64String;
+            image.style.display = 'block';
+            uploadSection.style.display = 'none';  // Hide upload section
         };
 
         reader.readAsDataURL(file);
@@ -20,5 +23,10 @@ window.onload = function() {
     if (window.location.hash) {
         const base64String = decodeURIComponent(window.location.hash.substring(1));
         image.src = base64String;
+        image.style.display = 'block';
+        uploadSection.style.display = 'none';  // Hide upload section
+    } else {
+        image.style.display = 'none';
+        uploadSection.style.display = 'block';  // Show upload section
     }
 };
